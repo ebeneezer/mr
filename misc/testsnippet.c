8b5576e0b6e827ad893ea32f1b46f257bf0c6603
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-05-30 11:54:41 +0200

    Kanban und Compiler-Smoke-Snippet aktualisieren

--- misc/testsnippet.c
+++ misc/testsnippet.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
 int main() {
+
+	int unused;
+         
+   pus("Hello world");
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    puts("Hello world");
+	
+	
+	
+   
+	
+	i="dumm";
 	return(0);
 }
