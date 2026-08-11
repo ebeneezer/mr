03dfd5d2bf9f7def6f4a42cc15de80e3860fd537
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-08-11 22:30:25 +0200

    Add Macro menu hotkey

--- app/MRMenuFactory.cpp
+++ app/MRMenuFactory.cpp
@@ -64,7 +64,7 @@ TSubMenu *createOtherMenu() {
 }
 
 TSubMenu *createMacroMenu() {
-	return &(*new TSubMenu("Macro", kbNoKey, hcMenuMacro) + *new TMenuItem("Macro ~L~ibrary...", cmMrOtherMacroLibrary, kbNoKey, hcNoContext) + newLine() + *new TMenuItem("~R~ecording start/stop", cmMrMacroToggleRecording, kbAltF10, hcNoContext, "AltF10"));
+	return &(*new TSubMenu("~M~acro", kbAltM, hcMenuMacro) + *new TMenuItem("Macro ~L~ibrary...", cmMrOtherMacroLibrary, kbNoKey, hcNoContext) + newLine() + *new TMenuItem("~R~ecording start/stop", cmMrMacroToggleRecording, kbAltF10, hcNoContext, "AltF10"));
 }
 
 TSubMenu *createHelpMenu() {
