21604a25e89155b322ed04e8fcfcbb9775b08684
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-08-15 19:16:00 +0200

    Release 0.2.19 for updater verification

--- app/MRVersion.cpp
+++ app/MRVersion.cpp
@@ -8,7 +8,7 @@
 #endif
 
 namespace {
-constexpr char kMrDisplayVersion[] = "0.2.18";
+constexpr char kMrDisplayVersion[] = "0.2.19";
 constexpr char kBuildLabel[] = " (build ";
 constexpr char kBuildSuffix[] = ")";
 constexpr char kSettingsVersionSetupKey[] = "SETTINGS_VERSION";
