4c438b87182e5c5798599f221c5b13fa7d4db47f
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-08-15 12:42:57 +0200

    Release 0.2.11 with 24-bit color management

--- app/MRVersion.cpp
+++ app/MRVersion.cpp
@@ -8,7 +8,7 @@
 #endif
 
 namespace {
-constexpr char kMrDisplayVersion[] = "0.2.10";
+constexpr char kMrDisplayVersion[] = "0.2.11";
 constexpr char kBuildLabel[] = " (build ";
 constexpr char kBuildSuffix[] = ")";
 constexpr char kSettingsVersionSetupKey[] = "SETTINGS_VERSION";
