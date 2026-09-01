6d46bc84de6894b76e2b16b201d260e75068c6d5
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-09-01 22:24:58 +0200

    Bump release version to 0.2.41

--- app/MRVersion.cpp
+++ app/MRVersion.cpp
@@ -8,7 +8,7 @@
 #endif
 
 namespace {
-constexpr char kMrDisplayVersion[] = "0.2.40";
+constexpr char kMrDisplayVersion[] = "0.2.41";
 constexpr char kBuildLabel[] = " (build ";
 constexpr char kBuildSuffix[] = ")";
 constexpr char kSettingsVersionSetupKey[] = "SETTINGS_VERSION";
