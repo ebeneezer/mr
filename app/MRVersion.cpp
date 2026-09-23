e4be4d97d84d4034d803191a87d5f4d1aa9a483e
Dr. Michael H. Raus <dr.michael.raus@gmail.com>
2026-09-23 20:31:59 +0200

    Prepare version 0.2.63 and refresh build dependencies
    - Set the displayed and packaged version to 0.2.63.
    - Track the direct header dependencies of the changed modules.

--- app/MRVersion.cpp
+++ app/MRVersion.cpp
@@ -8,7 +8,7 @@
 #endif
 
 namespace {
-constexpr char kMrDisplayVersion[] = "0.2.62";
+constexpr char kMrDisplayVersion[] = "0.2.63";
 constexpr char kBuildLabel[] = " (build ";
 constexpr char kBuildSuffix[] = ")";
 constexpr char kSettingsVersionSetupKey[] = "SETTINGS_VERSION";
